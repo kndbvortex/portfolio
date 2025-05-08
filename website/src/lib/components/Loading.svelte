78a42d294b52335f9e985c3a040b790285e0e48a
Durande Kamga <kamganguifo@gmail.com>
2025-05-09 00:52:10 +0200

    project on carousel

--- website/src/lib/components/Loading.svelte
+++ website/src/lib/components/Loading.svelte
@@ -44,26 +44,26 @@
 
     });
 </script>
-<div class="absolute top-0 w-full h-full font-SpaceGrotesk z-50 font-bold" id="view-loading">
+<div class="absolute top-0 w-full h-full font-SpaceGrotesk z-50 font-bold m-auto" id="view-loading">
     <div class="relative" bind:this={container}>
         <div
                 id="intro-slider"
                 class="h-screen p-10 bg-gray-50 absolute top-0 left-0 font-Inter z-10 w-full flex flex-col gap-10 tracking-tight"
         >
-            <h1 class="text-9xl" id="title-1">
+            <h1 class="text-5xl md:text-9xl" id="title-1">
                 Software Developper
             </h1>
-            <h1 class="text-9xl" id="title-2">
+            <h1 class="text-5xl md:text-9xl" id="title-2">
                 Data scientist
             </h1>
-            <h1 class="text-9xl" id="title-3">
+            <h1 class="text-5xl md:text-9xl" id="title-3">
                 Freelancer
             </h1>
         </div>
         <div class="h-screen flex bg-gray-950 justify-center place-items-center" id="background-black">
             <h1
                     id="welcome"
-                    class="text-9xl font-bold text-gray-100"
+                    class="text-5xl md:text-9xl font-bold text-gray-100"
             >
                 Welcome.
             </h1>
